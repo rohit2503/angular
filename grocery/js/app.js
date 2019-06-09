b95644f44654d0b5dd56cb9175bd182e96c4c254
rohit <rohitjaiswal2503@gmail.com>
2019-06-09 21:26:22 +0530

    Angular work and practice.

---
+++ grocery/js/app.js
@@ -0,0 +1,5 @@
+/**
+ * created by rohit jaiswal on 9th June 2019
+ * **/
+
+var app = angular.module("tutorialapp", ["tutorialCtrlModule"])
---
+++ grocery/js/controllers/tutorialCtrl.js
@@ -0,0 +1,49 @@
+angular.module("tutorialCtrlModule", [])
+
+.controller("TutorialCtrl", ["$scope", "Calculations", function ($scope, Calculations) {
+
+    // programming work here
+
+    $scope.tutorialObject = {};
+    $scope.tutorialObject.title = "Main Page";
+    $scope.tutorialObject.subtitle = "Sub title";
+
+    $scope.tutorialObject.bindOutput = 2
+
+    $scope.tutorialObject.firstname = "Rohit";
+    $scope.tutorialObject.lastname = "Jaiswal";
+
+    $scope.twotimes = function () {
+
+        $scope.tutorialObject.bindOutput = Calculations.twotimes($scope.tutorialObject.bindOutput);
+        
+    }
+
+
+}])
+
+
+.factory("Calculations", function () {
+
+    var myvar = [];
+
+    myvar.twotimes = function (a) {
+
+        return a*2;
+    };
+
+    return myvar;
+
+
+})
+
+.directive("welcomemessage",  function() {
+
+    return {
+
+        restrict : "AE",
+        template : "<div>Howdy how are you </div>"
+
+    }
+
+});
